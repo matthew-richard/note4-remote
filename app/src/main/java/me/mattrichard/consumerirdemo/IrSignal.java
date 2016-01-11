c31ba7af02d1350418d6aaa5b9d41c30e03e534f
mricha56 <mricha56@jhu.edu>
2016-01-10 22:27:08 -0500

    IrSignal can now read one pronto code from a given file

--- app/src/main/java/me/mattrichard/consumerirdemo/IrSignal.java
+++ app/src/main/java/me/mattrichard/consumerirdemo/IrSignal.java
@@ -10,8 +10,10 @@ import com.esotericsoftware.yamlbeans.YamlReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Created by Matt on 1/10/2016.
@@ -27,14 +29,17 @@ public class IrSignal {
         OTHER
     }
 
+    @SuppressWarnings("unchecked")
     public IrSignal(InputStream file) throws YamlException {
-        // TODO: Implement IrSignal initialization from pronto file
+        initialize();
 
         YamlReader reader = new YamlReader(new InputStreamReader(file));
         Map<String, Object> yaml = (Map<String,Object>) reader.read();
 
         deviceBrand = normalizeString((String)yaml.get("brand"));
-        deviceModel = normalizeString((String)yaml.get("model"));
+        if (yaml.containsKey("model"))
+            deviceModel = normalizeString((String)yaml.get("model"));
+        else deviceModel = "ANY";
         deviceType = IrDeviceType.valueOf(normalizeString((String)yaml.get("device"))
                 .toUpperCase());
 
@@ -43,13 +48,15 @@ public class IrSignal {
                        + "yaml files.");
             yaml = (Map<String, Object>) reader.read();
         }
-        action = Action.valueOf(normalizeString((String)yaml.get("action")));
+        action = Action.valueOf(normalizeString((String)yaml.get("action")).toUpperCase());
         Object pronto = yaml.get("pronto");
         processPronto(pronto);
     }
 
     private IrSignal(String devBrand, String devModel, IrDeviceType devType, Action action,
                      Object pronto) {
+        initialize();
+
         deviceBrand = devBrand;
         deviceModel = devModel;
         deviceType = devType;
@@ -67,24 +74,45 @@ public class IrSignal {
         mCIM = CIM;
     }
 
-    public static int getCycleDuration(int frequency) {
-        return Math.round(1000000.0f / frequency);
-    }
-
+    @SuppressWarnings("unchecked")
     private void processPronto(Object pronto) {
         // TODO: Implement processPronto()
 
         // check if list or string
-        Log.d(IrSignal.TAG, pronto.getClass().getName());
+        if (pronto instanceof String)
+            processOnePronto((String)pronto);
+        else for (String p : (List<String>)pronto)
+            processOnePronto(p);
+    }
+
+    private void processOnePronto(String pronto) {
+        Scanner scanner = new Scanner(pronto);
+
+        // Calculate transmission frequency from second number, converting from Pronto units
+        // to Hz.
+        scanner.next();
+        long frequency = Math.round(1000000.0/(Integer.parseInt(scanner.next(), 16) * 0.241246));
+        frequencies.add((int)frequency);
+        scanner.next();
+        scanner.next();
+
+        // Read on/off burst durations from remaining numbers and convert them from
+        // cycles to milliseconds.
+        int cycleLength = Math.round(1000000.0f / frequency);
+        String[] tokens = scanner.nextLine().trim().split(" ");
+        int[] pattern = new int[tokens.length];
+        for (int i = 0; i < pattern.length; i++)
+            pattern[i] = Integer.parseInt(tokens[i], 16) * cycleLength;
+        patterns.add(pattern);
     }
 
     public boolean isMacro() {
         return patterns.size() > 1;
     }
 
-    public void transmit() throws Exception {
+    public void transmit() throws RuntimeException {
         if (mCIM == null) {
-            throw new Exception(
+            throw new RuntimeException(
                 "ERROR: No transmitter provided. Cannot transmit IrSignal " + this.toString()
             );
         }
@@ -95,11 +123,10 @@ public class IrSignal {
     }
 
     private void initialize() {
-        patterns = null;
+        patterns = new ArrayList<int[]>();
         deviceBrand = null;
         deviceModel = null;
-        frequencies = null;
-        mCIM = null;
+        frequencies = new ArrayList<Integer>();
         action = null;
         deviceType = null;
     }
@@ -136,7 +163,7 @@ public class IrSignal {
     }
 
 
-    static ConsumerIrManager mCIM;
+    static ConsumerIrManager mCIM = null;
 
     /* Signal data */
     List<int[]> patterns;
